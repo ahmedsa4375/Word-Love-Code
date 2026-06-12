040b66b571457812e1e76a66e06d42cce87fedb9
khafaga <150396109+ahmedsa4375@users.noreply.github.com>
2026-06-12 16:29:56 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||You|Are|Ready?||#rectangle|");
+       S.UI.simulate("|#countdown 3||I|Love|Mariam||#rectangle|");
     }
 
     // Add a flag to track animation completion
